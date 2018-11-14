4084fcc5c663a2d36435d0f226d9cde1595863df
screspia <s.crespi.a@gmail.com>
2018-11-14 11:23:36 +0100

    Add files via upload

--- Dataflow-template.py
+++ Dataflow-template.py
@@ -1,15 +1,12 @@
 import datetime
 
 from airflow import models
-from airflow.contrib.operators import bigquery_get_data
-from airflow.contrib.operators import bigquery_operator
+from airflow.contrib.operators import dataflow_operator
 
 
 yesterday = datetime.datetime.combine(
     datetime.datetime.today() - datetime.timedelta(1),
     datetime.datetime.min.time())
-
-bq_dataset_name = 'test.users_data_last'
 
 default_dag_args = {
     'start_date': yesterday,
@@ -19,25 +16,28 @@ default_dag_args = {
     'email_on_retry': False,
     # If a task fails, retry it once after waiting at least 5 minutes
     'retries': 1,
-    'retry_delay': datetime.timedelta(minutes=5)
+    'retry_delay': datetime.timedelta(minutes=5),
+    'dataflow_default_options': {
+            'project': 'my-test-project-218908',
+            'zone': 'europe-west1-d',
+            'tempLocation': 'gs://staging-bucket-tes/staging'
+            }
 }
 
 
 # [START bigquery_extracton_test]
 with models.DAG(
-        'users_data_bigquery_load_test',
+        'dataflow_test',
         # Continue to run DAG once per day
         schedule_interval=datetime.timedelta(days=1),
         default_args=default_dag_args) as dag:
     # [END bigquery_extracton_test]
 
-    bq_extraction_test = bigquery_operator.BigQueryOperator(
-            task_id='bq_extraction_test_query',
-            bql="""
-            SELECT *
-            FROM `test.users_data` 
-            """,
-            use_legacy_sql=False,
-            destination_dataset_table=bq_dataset_name)
+    first_dataflow = dataflow_operator.DataflowTemplateOperator(
+            task_id='dataflow_test',
+            template='gs://staging-bucket-tes/templates/PublisherDemo',
+            gcp_conn_id='google_cloud_default',
+            dag=dag
+            )
 
-bq_extraction_test 
+first_dataflow
